6f1d1270ffb698660a4d9e1ae4ee5569092737b5
MuziShenZhen <LX-PC12@DESKTOP-C70NJE3>
2025-07-07 17:21:21 +0800

    初始文件

---
+++ App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Company.Application.Share.Prism;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp2.Views;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return new MainWindow();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+            //moduleCatalog.AddModule<ApplicatoinMainModule>();
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new DirectoryModuleCatalog()
+            {
+                // 配置本地模块目录
+                ModulePath = ModuleNames.ModulePath
+            };
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.ViewModels
+{
+    internal class MainWindowViewModel:BindableBase
+    {
+        private string title = "ni好";
+        public string Title
+        {
+            get { return title; }
+            set { title = value;  RaisePropertyChanged(); }
+        }
+    }
+}
